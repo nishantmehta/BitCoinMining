946925f27c42f5e5c90ca9f848d7dec774170799
nishant <nishantmehta.n@gmail.com>
2014-09-16 18:10:01 -0400

    master included in the main file

--- src/com/bitcoin/BitCoinMining.scala
+++ src/com/bitcoin/BitCoinMining.scala
@@ -37,6 +37,33 @@ object BitCoinMining extends App {
     master ! ScheduleMining
   }
 
+  class BitCoinMaster(numberOfWorkers: Int, prefix: String, timeInMinutes: Int) extends Actor {
+
+    val workerRouter = context.actorOf(Props[BitCoinMiner].withRouter(RoundRobinRouter(numberOfWorkers)), name = "workerRouter")
+    var bitCoins: SortedMap[String, String] = SortedMap[String, String]()(implicitly[Ordering[String]].reverse)
+    var finishedCounter = 0
+    def receive = {
+      case ScheduleMining =>
+        for (i <- 0 until numberOfWorkers) {
+          val postfix = "Worker" + i
+          workerRouter ! BitCoinWork("nishantmehta.n", postfix, timeInMinutes)
+        }
+      case BitCoinResult(bitCoinString, hashValue) => {
+        bitCoins += (hashValue -> bitCoinString)
+        println(bitCoinString + "\t" + hashValue)
+      }
+      case FinishedMining => {
+        finishedCounter = finishedCounter + 1
+        if (finishedCounter == numberOfWorkers - 1) {
+          printBitCoins(bitCoins)
+          println("stopping master" + "with " + bitCoins.size)
+          context.system.shutdown()
+        }
+      }
+    }
+
+  }
+
   def hex_digest(s: String): String = {
     val sha = MessageDigest.getInstance("SHA-256")
     sha.digest(s.getBytes)
